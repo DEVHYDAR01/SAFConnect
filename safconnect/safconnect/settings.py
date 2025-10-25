eb9261d530187424a5e67fa601d99db415129a3a
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-10-25 11:38:57 +0100

    This commit will push scrap the database engine as dbsqlite to postgressql note postgres is running on a docker container

--- safconnect/safconnect/settings.py
+++ safconnect/safconnect/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'safconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',  # The database name you set in the Docker container
+        'USER': 'django_user',  # The PostgreSQL user you set
+        'PASSWORD': os.getenv("django-postgres-password"),  # The PostgreSQL user password you set
+        'HOST': 'localhost',  # The name of the PostgreSQL container
+        'PORT': '5432',  # The default PostgreSQL port
     }
 }
 
